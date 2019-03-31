3637a1a8ee88af91c1ae5e16496adee9b635e250
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 19:01:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
       if supplies.include?("bbq")
-        has_bbq << holiday.to_s
+        has_bbq << holiday
       end
     end
   end
