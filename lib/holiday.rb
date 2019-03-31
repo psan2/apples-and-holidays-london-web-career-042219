a84767896a5089d0fd1365af0bfa2b3650cc967a
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 19:00:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,5 +68,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  has_bbq = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supplies|
+      if supplies.include?("bbq")
+        has_bbq << holiday.to_s
 
 end
