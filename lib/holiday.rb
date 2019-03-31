361102df6eb6fc308218b4d0d65a58951013f7a1
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 18:41:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season|
+    season_supplies = []
+    holiday_hash[season].each do |holiday, supply|
+      season_supplies << holiday.values
 end
 
 def all_holidays_with_bbq(holiday_hash)
