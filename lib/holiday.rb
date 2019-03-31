1698002ef3ce4dff6b37b40dadf90b0a4badc1d9
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 18:47:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, supply|
-      puts "#{holiday.to_s.split(" ").capitalize.join(" ")}: #{supply.join(", ")}"
+      puts "#{holiday.split(" ").capitalize.join(" ").to_s}: #{supply.join(", ")}"
     end
   end
 end
